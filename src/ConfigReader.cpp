4d5774c0392eb873634de093d0ec24b266ea066b
Andrew Halaney <ajhalaney@gmail.com>
2017-03-21 16:14:42 -0500

    Created basic skeleton for ConfigReader. Considering pulling in jsoncpp to use json as the config file type. It will make writing the file from python simpler and it will give me some piece of mind when doing the file I/O

--- src/ConfigReader.cpp
+++ src/ConfigReader.cpp
@@ -1,4 +1,49 @@
+#include <string>
 #include "./ConfigReader.h"
 
 
+ConfigReader::ConfigReader(const char *fileName)
+	: configFile(fileName)
+{
+	read();
+}
 
+
+void ConfigReader::reRead()
+{
+
+}
+
+
+unsigned int ConfigReader::getStartTime() const
+{
+	return startTime;
+}
+
+
+int ConfigReader::getExpectedLowValue() const
+{
+	return expectedLowValue;
+}
+
+int ConfigReader::getExpectedHighValue() const
+{
+	return expectedHighValue;
+}
+
+
+unsigned int ConfigReader::getSampleRate() const
+{
+	return sampleRate;
+}
+
+unsigned int ConfigReader::getTimeToRead() const
+{
+	return timeToRead;
+}
+
+
+void ConfigReader::read()
+{
+
+}
--- src/ConfigReader.h
+++ src/ConfigReader.h
@@ -1,4 +1,27 @@
 #ifndef CONFIGREADER_H_
 #define CONFIGREADER_H_
 // This should really be replaced by some open-source json reader
+#include <string>
+
+class ConfigReader
+{
+public:
+	ConfigReader(const char *configFile);  // Reads config upon construction
+	void reRead();  // Reread the config file
+	unsigned int getStartTime() const;  // Gets the time to start reading from the sensor
+	int getExpectedLowValue() const;  // Gets the expected lowest value the sensor should see
+	int getExpectedHighValue() const;  // Gets the expected highest value sensor should see
+	unsigned int getSampleRate() const;  // Gets the sample rate in Hz
+	unsigned int getTimeToRead() const;  // Gets how long the sensor should read
+
+private:
+	void read();
+	const std::string configFile;
+	unsigned int startTime;
+	int expectedLowValue;
+	int expectedHighValue;
+	unsigned int sampleRate;
+	unsigned int timeToRead;
+};
+
 #endif /* CONFIGREADER_H_ */
